6723e038421e39e8557b4293a51d152a64c538b7
Lexographics <zbamgamesinfo@gmail.com>
2021-09-18 20:14:54 +0300

    Add window icons
    see resources/Mechanic/credits.txt

--- src/renderer/window/window.cpp
+++ src/renderer/window/window.cpp
@@ -6,6 +6,9 @@
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
 namespace Ease
 {
    Window::Window(const char* title, int w, int h)
@@ -25,6 +28,13 @@ namespace Ease
          glfwTerminate();
          exit(ERROR_GLFW_WINDOW_CREATION);
       }
+
+
+      // set window icon
+      GLFWimage images[1]; 
+      images[0].pixels = stbi_load("resources/Mechanic/idle_1.png", &images[0].width, &images[0].height, 0, 4); //rgba channels 
+      glfwSetWindowIcon(windowPointer, 1, images); 
+      stbi_image_free(images[0].pixels);
    }
 
 
