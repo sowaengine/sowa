43d11339be9df747d78de74c6963a90bbc95bd73
Lexographics <zbamgamesinfo@gmail.com>
2022-06-04 20:12:49 +0300

    Add YAML overloads for glm vec2, vec3 and vec4

--- src/Utils/YAML.cpp
+++ src/Utils/YAML.cpp
@@ -14,6 +14,25 @@ namespace YAML
       out << YAML::BeginSeq << vec.x << vec.y << vec.z << vec.w << YAML::EndSeq;
       return out;
    }
+
+   YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec2& vec)
+   {
+      out << YAML::Flow;
+      out << YAML::BeginSeq << vec.x << vec.y << YAML::EndSeq;
+      return out;
+   }
+   YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec3& vec)
+   {
+      out << YAML::Flow;
+      out << YAML::BeginSeq << vec.x << vec.y << vec.z << YAML::EndSeq;
+      return out;
+   }
+   YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec4& vec)
+   {
+      out << YAML::Flow;
+      out << YAML::BeginSeq << vec.x << vec.y << vec.z << vec.w << YAML::EndSeq;
+      return out;
+   }
 } // namespace YAML
 
 namespace Ease::Utils
--- src/Utils/YAML.h
+++ src/Utils/YAML.h
@@ -5,6 +5,7 @@
 
 #include "imgui-docking/imgui.h"
 #include "yaml-cpp/yaml.h"
+#include "glm/glm.hpp"
 
 namespace YAML
 {
@@ -53,10 +54,81 @@ namespace YAML
             return true;
         }
     };
+
+    template<>
+    struct convert<glm::vec2>
+    {
+        static Node encode(const glm::vec2& vec)
+        {
+            Node node;
+            node.push_back(vec.x);
+            node.push_back(vec.y);
+            return node;
+        }
+        static bool decode(const Node& node, glm::vec2& vec)
+        {
+            if(!node.IsSequence() || node.size() != 2)
+                return false;
+            
+            vec.x = node[0].as<float>();
+            vec.y = node[1].as<float>();
+            return true;
+        }
+    };
+    template<>
+    struct convert<glm::vec3>
+    {
+        static Node encode(const glm::vec3& vec)
+        {
+            Node node;
+            node.push_back(vec.x);
+            node.push_back(vec.y);
+            node.push_back(vec.z);
+            return node;
+        }
+        static bool decode(const Node& node, glm::vec3& vec)
+        {
+            if(!node.IsSequence() || node.size() != 3)
+                return false;
+            
+            vec.x = node[0].as<float>();
+            vec.y = node[1].as<float>();
+            vec.z = node[2].as<float>();
+            return true;
+        }
+    };
+    template<>
+    struct convert<glm::vec4>
+    {
+        static Node encode(const glm::vec4& vec)
+        {
+            Node node;
+            node.push_back(vec.x);
+            node.push_back(vec.y);
+            node.push_back(vec.z);
+            node.push_back(vec.w);
+            return node;
+        }
+        static bool decode(const Node& node, glm::vec4& vec)
+        {
+            if(!node.IsSequence() || node.size() != 4)
+                return false;
+            
+            vec.x = node[0].as<float>();
+            vec.y = node[1].as<float>();
+            vec.z = node[2].as<float>();
+            vec.w = node[3].as<float>();
+            return true;
+        }
+    };
     
 
     YAML::Emitter& operator<<(YAML::Emitter& out, const ImVec2& vec);
     YAML::Emitter& operator<<(YAML::Emitter& out, const ImVec4& vec);
+
+    YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec2& vec);
+    YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec3& vec);
+    YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec4& vec);
 } // namespace YAML
 
 
