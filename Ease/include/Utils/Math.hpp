a523f8b7bd60900ef2813bfb3f5a897a6cbcf2a5
Lexographics <zbamgamesinfo@gmail.com>
2022-09-28 04:12:10 +0300

    Math: Add Length, Clamp, Angle methods to Vec2

--- Ease/include/Utils/Math.hpp
+++ Ease/include/Utils/Math.hpp
@@ -14,6 +14,15 @@ struct Vec2 {
 	Vec2()
 		: x(0.f), y(0.f) {
 	}
+
+	/// @brief Returns magnitude of vector
+	float Length();
+
+	/// @brief returns a vector with same direction with magnitude @param length
+	Vec2 Clamp(float length);
+
+	/// @brief returns angle of vector in radians starting from right and rotates clockwise. (1, 0) -> 0 and (0, 1) -> PI / 2
+	float Angle();
 };
 } // namespace Ease
 
