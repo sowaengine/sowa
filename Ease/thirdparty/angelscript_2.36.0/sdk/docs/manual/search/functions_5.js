e3ef5815d7c8783145f86ed3dc04c3171388ed4c
Lexographics <zbamgamesinfo@gmail.com>
2022-11-05 19:27:47 +0300

    Remove lib angelscript

--- Ease/thirdparty/angelscript_2.36.0/sdk/docs/manual/search/functions_5.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['findnextlinewithcode_1018',['FindNextLineWithCode',['../classas_i_script_function.html#a30dc23991856a13f59e682b3b1498e2f',1,'asIScriptFunction']]],
-  ['finishdeserialization_1019',['FinishDeserialization',['../classas_i_script_context.html#ae804cb97a93a4524c0cd374d82ebaa9c',1,'asIScriptContext']]],
-  ['forwardgcenumreferences_1020',['ForwardGCEnumReferences',['../classas_i_script_engine.html#abe95ce0e45d914fec478fa112a7bb8dd',1,'asIScriptEngine']]],
-  ['forwardgcreleasereferences_1021',['ForwardGCReleaseReferences',['../classas_i_script_engine.html#a60cdec608a18f6ebc0aebe29a143183f',1,'asIScriptEngine']]]
-];
