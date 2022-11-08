fefaf4487601023fe218f41c3f4378fda873761b
Lexographics <zbamgamesinfo@gmail.com>
2022-11-08 14:27:33 +0300

    Lua: Add Gui/Math/Singleton usertypes

---
+++ Ease/src/Servers/ScriptServer/Lua/Classes/LuaGui.cpp
@@ -0,0 +1,156 @@
+#include "Servers/ScriptServer/LuaScriptServer.hpp"
+
+#include "Servers/GuiServer/GuiServer.hpp"
+
+namespace Ease {
+
+void LuaScriptServer::RegisterGuiServer() {
+	_pState->new_enum("gui_window_flags",
+					  "none", WindowFlags_None,
+					  "no_resize", WindowFlags_NoResize,
+					  "no_move", WindowFlags_NoMove,
+					  "no_bring_to_front_on_focus", WindowFlags_NoBringToFrontOnFocus,
+					  "no_nav_focus", WindowFlags_NoNavFocus,
+					  "no_docking", WindowFlags_NoDocking,
+					  "no_title_bar", WindowFlags_NoTitleBar,
+					  "no_collapse", WindowFlags_NoCollapse,
+					  "menu_bar", WindowFlags_MenuBar,
+					  "no_background", WindowFlags_NoBackground);
+
+	_pState->new_enum("gui_style_var",
+					  "none", StyleVar::None,
+					  "window_padding", StyleVar::WindowPadding,
+					  "window_rounding", StyleVar::WindowRounding);
+
+	_pState->new_enum("gui_mouse_button",
+					  "none", GuiMouseButton::None,
+					  "left", GuiMouseButton::Left,
+					  "right", GuiMouseButton::Right,
+					  "middle", GuiMouseButton::Middle);
+
+	_pState->new_usertype<GuiServer>(
+		"GuiServer",
+
+		"new", sol::no_constructor,
+		"get", [this]() -> Ease::GuiServer & { return *this->_Context.GetSingleton<Ease::GuiServer>(Ease::Server::GUISERVER); },
+
+		"begin_window", &GuiServer::BeginWindow,
+		"end_window", &GuiServer::EndWindow,
+
+		"begin_child",
+		sol::overload(
+			&GuiServer::BeginChild,
+			[](GuiServer &self, const std::string &id) { return self.BeginChild(id, 0, 0); },
+			[](GuiServer &self, const std::string &id, double w, double h) { return self.BeginChild(id, static_cast<int>(w), static_cast<int>(h)); }),
+
+		"end_child", &GuiServer::EndChild,
+
+		"set_next_window_size", [](GuiServer &self, double w, double h) -> void { self.SetNextWindowSize(static_cast<int>(w), static_cast<int>(h)); },
+		"set_next_window_pos", [](GuiServer &self, double x, double y) -> void { self.SetNextWindowPos(static_cast<int>(x), static_cast<int>(y)); },
+
+		"push_style_var",
+		sol::overload(
+			[](GuiServer &self, StyleVar var, double value) -> void { self.PushStyleVar(var, static_cast<float>(value)); },
+			[](GuiServer &self, StyleVar var, Vec2 value) -> void { self.PushStyleVar(var, value); }),
+
+		"pop_style_var", &GuiServer::PopStyleVar,
+
+		"begin_main_menu_bar", &GuiServer::BeginMainMenuBar,
+		"end_main_menu_bar", &GuiServer::EndMainMenuBar,
+		"begin_menu", &GuiServer::BeginMenu,
+		"end_menu", &GuiServer::EndMenu,
+		"menu_item",
+		sol::overload(
+			&GuiServer::MenuItem,
+			[](GuiServer &self, const std::string &label) -> bool { return self.MenuItem(label, ""); }),
+
+		"register_panel", &GuiServer::RegisterPanel,
+		"draw_view_bar", &GuiServer::DrawViewbar,
+
+		"check_box", [](GuiServer &self, const std::string &label, bool value, sol::this_state L) -> sol::variadic_results {
+				bool val = value;
+				bool ret = self.Checkbox(label, val);
+				
+				sol::variadic_results values;
+				values.push_back( {L, sol::in_place_type<bool>, val} );
+				values.push_back( {L, sol::in_place_type<bool>, ret} );
+
+				return values; },
+
+		"input_text", [](GuiServer &self, const std::string &label, const std::string &value, sol::this_state L) -> sol::variadic_results {
+			std::string val = value;
+			bool ret = self.InputText(label, val);
+
+			sol::variadic_results values;
+			values.push_back( {L, sol::in_place_type<std::string>, val} );
+			values.push_back( {L, sol::in_place_type<bool>, ret} );
+
+			return values; },
+
+		"drag_float", [](GuiServer &self, const std::string &label, float value, sol::this_state L) -> sol::variadic_results {
+			float val = value;
+			bool ret = self.DragFloat(label, val);
+
+			sol::variadic_results values;
+			values.push_back( {L, sol::in_place_type<float>, val} );
+			values.push_back( {L, sol::in_place_type<bool>, ret} );
+
+			return values; },
+
+		"drag_float2", [](GuiServer &self, const std::string &label, Vec2 value, sol::this_state L) -> sol::variadic_results {
+			Vec2 val = value;
+			bool ret = self.DragFloat2(label, val.x, val.y);
+
+			sol::variadic_results values;
+			values.push_back( {L, sol::in_place_type<Vec2>, val} );
+			values.push_back( {L, sol::in_place_type<bool>, ret} );
+
+			return values; },
+
+		"setup_dockspace", &GuiServer::SetupDockspace,
+
+		"begin_footer", &GuiServer::BeginFooter,
+		"end_footer", &GuiServer::EndFooter,
+
+		"text", &GuiServer::Text,
+		"text_unformatted", &GuiServer::TextUnformatted,
+
+		"show_demo_window", &GuiServer::ShowDemoWindow,
+
+		"draw_filesystem", &GuiServer::DrawFilesystem,
+		"draw_play_button", &GuiServer::DrawPlayButton,
+		"separator", &GuiServer::Separator,
+		"draw_frame", &GuiServer::DrawFrame,
+
+		"draw_scene", &GuiServer::DrawScene,
+
+		"is_window_hovered", &GuiServer::IsWindowHovered,
+		"is_mouse_clicked", &GuiServer::IsMouseClicked,
+		"is_mouse_pressed", &GuiServer::IsMousePressed,
+
+		"open_context_menu", &GuiServer::OpenContextMenu,
+		"begin_context_menu", &GuiServer::BeginContextMenu,
+		"end_context_menu", &GuiServer::EndContextMenu,
+
+		"get_available_width", &GuiServer::GetAvailableWidth,
+		"get_available_height", &GuiServer::GetAvailableHeight,
+		"get_title_height", &GuiServer::GetTitleHeight,
+
+		"set_scroll_ratio_y", &GuiServer::SetScrollRatioY,
+
+		"same_line", &GuiServer::SameLine,
+
+		"header", &GuiServer::Header,
+		"indent",
+		sol::overload(&GuiServer::Indent,
+					  [](GuiServer &self) { self.Indent(0.f); }),
+
+		"unindent",
+		sol::overload(&GuiServer::Unindent,
+					  [](GuiServer &self) { self.Unindent(0.f); }),
+
+		"get_mouse_position", &GuiServer::GetMousePosition,
+
+		"button", [](GuiServer &self, const std::string &label) -> bool { return self.Button(label); });
+}
+} // namespace Ease
---
+++ Ease/src/Servers/ScriptServer/Lua/Classes/LuaSingleton.cpp
@@ -0,0 +1,82 @@
+#include "Servers/ScriptServer/LuaScriptServer.hpp"
+
+#include "Core/Application.hpp"
+
+#include "Utils/Math.hpp"
+#include "Utils/String.hpp"
+
+#include "fmt/args.h"
+#include "fmt/core.h"
+
+namespace Ease {
+void LuaScriptServer::RegisterSingleton() {
+	_pState->new_usertype<Application>(
+		"Application",
+
+		"new", sol::no_constructor,
+		"get", [this]() -> Ease::Application & { return *this->_Context.GetSingleton<Ease::Application>(Ease::Server::APPLICATION); },
+
+		"get_current_scene", &Ease::Application::GetCurrentScene,
+
+		"selected_entity",
+		sol::property(
+			[](Ease::Application &self) -> Ease::Entity & { return self.SelectedEntity(); },
+			[](Ease::Application &self, Ease::Entity &entity) -> void { self.SelectedEntity() = entity; }));
+
+	_pState->new_usertype<Window>(
+		"Window",
+
+		"new", sol::no_constructor,
+		"get", [this]() -> Ease::Window & { return this->_Context.GetSingleton<Ease::Application>(Ease::Server::APPLICATION)->GetWindow(); },
+
+		"get_window_size", [](Window &self) -> Vec2 { return Vec2((float)self.GetWindowWidth(), (float)self.GetWindowHeight()); });
+
+	_pState->create_named_table("Debug",
+								"log",
+								[](sol::variadic_args args) {
+									if (args.size() == 0)
+										return;
+
+									std::string format{""};
+									format = args[0].as<std::string>();
+
+									fmt::dynamic_format_arg_store<fmt::format_context> store;
+
+									sol::variadic_args::iterator it = args.begin();
+									++it; // skip format arg
+									for (; it != args.end(); ++it) {
+										sol::type type = (*it).get_type();
+
+										if (type == sol::type::boolean) {
+											bool value = (*it).as<bool>();
+											if (value)
+												store.push_back("true");
+											else
+												store.push_back("false");
+										} else if (type == sol::type::number) {
+											double value = (*it).as<double>();
+											store.push_back(value);
+										} else if (type == sol::type::string) {
+											std::string value = (*it).as<std::string>();
+											store.push_back(Format("\"{}\"", value));
+										} else if (type == sol::type::nil) {
+											store.push_back("nil");
+										} else if (type == sol::type::userdata) {
+											sol::userdata val = (*it).as<sol::userdata>();
+											store.push_back("userdata");
+										} else {
+											store.push_back(Format("<unknown_t:{}>", (int)type));
+										}
+									}
+
+									std::string msg = "FORMAT ERROR!";
+									try {
+										msg = fmt::vformat(format, store);
+									} catch (const std::exception &e) {
+										Debug::Error("String Format error: {}", e.what());
+									}
+
+									Debug::Log(msg);
+								});
+}
+} // namespace Ease
