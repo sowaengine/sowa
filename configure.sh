a332b9c605cbfaea20afae2ae268647eb4d3fc95
Lexographics <zbamgamesinfo@gmail.com>
2023-04-08 19:10:50 +0300

    Separate configure and build commands

--- configure.sh
+++ configure.sh
@@ -1,1 +1,3 @@
-cmake -S . -B build
+cmake -S thirdparty/manual/zlib-1.2.13 -B build/thirdparty/zlib -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=bin/zlib
+
+cmake -S . -B build/sowa/ -DBUILD_SHARED_LIBS=OFF
--- make.sh
+++ make.sh
@@ -1,6 +1,3 @@
-cmake -S thirdparty/manual/zlib-1.2.13 -B build/thirdparty/zlib -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=bin/zlib
 cmake --build build/thirdparty/zlib --parallel 4 --target install
 
-
-cmake -S . -B build/sowa/ -DBUILD_SHARED_LIBS=OFF
 cmake --build build/sowa/ --parallel 2 --target all
