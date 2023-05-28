9dfa8f908638176d667fa9faf11730a95bdf02ef
Lexographics <zbamgamesinfo@gmail.com>
2023-05-29 00:30:11 +0300

    Add serializer

--- Sowa/App/src/serialize/document.cpp
+++ Sowa/App/src/serialize/document.cpp
@@ -1,31 +1,41 @@
 #include "document.hpp"
 
 #include "toml.hpp"
+#include <sstream>
 
-namespace sowa
-{
-    SerializeDocument::SerializeDocument(toml::parse_result doc) {
-        m_doc = doc;
-    }
-    SerializeDocument::SerializeDocument(toml::node& doc) {
-        toml::table *table = doc.as_table();
-        if(nullptr != table)
-            m_doc = *table;
-    }
-    SerializeDocument::~SerializeDocument() {
+namespace sowa {
+SerializeDocument::SerializeDocument(toml::table doc) {
+	m_doc = doc;
+}
+SerializeDocument::SerializeDocument(toml::node &doc) {
+	toml::table *table = doc.as_table();
+	if (nullptr != table)
+		m_doc = *table;
+}
+SerializeDocument::~SerializeDocument() {
+}
 
-    }
+size_t SerializeDocument::Size() {
+	return m_doc.size();
+}
 
-    size_t SerializeDocument::Size() {
-        return m_doc.size();
-    }
+SerializeDocument SerializeDocument::Table(const std::string &name) {
+	toml::table *table = m_doc[name].as_table();
+	if (nullptr != table && table->type() == toml::node_type::table) {
+		return SerializeDocument(*table);
+	}
+	return SerializeDocument();
+}
 
-    SerializeDocument SerializeDocument::Table(const std::string& name) {
-        toml::table* table = m_doc[name].as_table();
-        if(nullptr != table && table->type() == toml::node_type::table) {
-            return SerializeDocument(*table);
-        }
-        return SerializeDocument();
-        
-    }
+std::string SerializeDocument::Serialize() {
+	std::stringstream ss;
+	ss << m_doc;
+	return ss.str();
+}
+
+template <>
+SerializeDocument &SerializeDocument::Set(const std::string &name, const SerializeDocument &value) {
+	m_doc.insert_or_assign(name, value.m_doc);
+	return *this;
+}
 } // namespace sowa
--- Sowa/App/src/serialize/document.hpp
+++ Sowa/App/src/serialize/document.hpp
@@ -10,12 +10,12 @@ class TomlSerializer;
 
 class SerializeDocument {
   public:
-    toml::table_iterator begin() {
-        return m_doc.begin();
-    }
-    toml::table_iterator end() {
-        return m_doc.end();
-    }
+	toml::table_iterator begin() {
+		return m_doc.begin();
+	}
+	toml::table_iterator end() {
+		return m_doc.end();
+	}
 
 	template <typename T>
 	SerializeDocument &Value(const std::string &name, T &value) {
@@ -28,21 +28,32 @@ class SerializeDocument {
 		return *this;
 	}
 
+	template <typename T>
+	SerializeDocument &Set(const std::string &name, const T &value) {
+		m_doc.insert_or_assign(name, value);
+		return *this;
+	}
+
 	SerializeDocument Table(const std::string &name);
-	SerializeDocument operator[](size_t i);
+	// SerializeDocument operator[](size_t i);
 
 	size_t Size();
+
+	std::string Serialize();
 
   public:
 	friend class TomlSerializer;
 
-	SerializeDocument(toml::parse_result);
-    SerializeDocument(toml::node&);
+	SerializeDocument(toml::table);
+	SerializeDocument(toml::node &);
 	SerializeDocument() = default;
 	~SerializeDocument();
 
-	toml::parse_result m_doc;
+	toml::table m_doc;
 };
+
+template <>
+SerializeDocument &SerializeDocument::Set(const std::string &name, const SerializeDocument &value);
 
 } // namespace sowa
 
