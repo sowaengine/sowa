58eedb783992bcec6182b4e4b3471b0914978804
Lexographics <zbamgamesinfo@gmail.com>
2023-06-27 16:03:10 +0300

    Add general graphics header

---
+++ src/core/graphics.hxx
@@ -0,0 +1,13 @@
+#ifndef SW_GRAPHICS_HXX
+#define SW_GRAPHICS_HXX
+#pragma once
+
+#define GLFW_INCLUDE_NONE
+
+#define GLSL(x) "#version 300 es\n" #x
+
+#include "rendering/model.hxx"
+#include "rendering/model_builder.hxx"
+#include "rendering/shader.hxx"
+
+#endif // SW_GRAPHICS_HXX
