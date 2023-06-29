9d00707a125fe9b9e71a8233f104ec75184e7a88
Lexographics <zbamgamesinfo@gmail.com>
2023-06-30 02:15:08 +0300

    Add toml serializer && toml_document

---
+++ src/data/toml_document.cxx
@@ -0,0 +1,114 @@
+#include "toml_document.hxx"
+
+#include <toml.hpp>
+
+#include <iostream>
+
+struct toml_document_data {
+	toml::table table;
+
+	static toml_document_data &get(void *internal) {
+		return *reinterpret_cast<toml_document_data *>(internal);
+	}
+};
+
+toml_document::toml_document() {
+	m_internal = new toml_document_data;
+}
+
+toml_document::toml_document(const toml_document &doc) {
+	m_internal = new toml_document_data;
+	toml_document_data::get(m_internal).table = toml_document_data::get(doc.m_internal).table;
+}
+
+toml_document::toml_document(const char *path) {
+	m_internal = new toml_document_data;
+	LoadFile(path);
+}
+
+toml_document::~toml_document() {
+	delete reinterpret_cast<toml_document_data *>(m_internal);
+}
+
+Error toml_document::LoadFile(const char *path) {
+	try {
+		toml_document_data::get(m_internal).table = toml::parse_file(path);
+	} catch (const std::exception &ex) {
+		return ERR_FAILED;
+	}
+	return OK;
+}
+
+Error toml_document::Serialize(std::string &str) {
+	std::stringstream ss;
+	ss << toml_document_data::get(m_internal).table << std::endl;
+
+	str = ss.str();
+	return OK;
+}
+
+template <>
+int toml_document::Value(const char *key, int fallback) {
+	return toml_document_data::get(m_internal).table[key].value_or(fallback);
+}
+
+template <>
+toml_document &toml_document::Set(const char *key, int value) {
+	toml_document_data::get(m_internal).table.insert_or_assign(key, value);
+	return *this;
+}
+
+template <>
+int64_t toml_document::Value(const char *key, int64_t fallback) {
+	return toml_document_data::get(m_internal).table[key].value_or(fallback);
+}
+
+template <>
+toml_document &toml_document::Set(const char *key, int64_t value) {
+	toml_document_data::get(m_internal).table.insert_or_assign(key, value);
+	return *this;
+}
+
+template <>
+std::string toml_document::Value(const char *key, std::string fallback) {
+	return toml_document_data::get(m_internal).table[key].value_or(fallback);
+}
+
+template <>
+toml_document &toml_document::Set(const char *key, std::string value) {
+	toml_document_data::get(m_internal).table.insert_or_assign(key, value);
+	return *this;
+}
+
+template <>
+toml_document &toml_document::Set(const char *key, const char *value) {
+	toml_document_data::get(m_internal).table.insert_or_assign(key, value);
+	return *this;
+}
+
+template <>
+toml_document &toml_document::Set(const char *key, toml_document value) {
+	toml_document_data::get(m_internal).table.insert_or_assign(key, toml_document_data::get(value.m_internal).table);
+	return *this;
+}
+
+toml_document toml_document::operator[](const char *v) {
+
+	toml_document doc;
+	toml::table *t = toml_document_data::get(m_internal).table[v].as_table();
+	if (nullptr != t) {
+		toml_document_data::get(doc.m_internal).table = *t;
+	}
+
+	return doc;
+}
+
+toml_document &toml_document::operator=(const toml_document &o) {
+	toml_document_data::get(m_internal).table = toml_document_data::get(o.m_internal).table;
+	return *this;
+}
+
+toml_document &toml_document::operator=(toml_document &&o) {
+	toml_document_data::get(m_internal).table = toml_document_data::get(o.m_internal).table;
+	return *this;
+}
