131637dd7d8b3a882a44d062463ef71d32a02f8e
Lexographics <zbamgamesinfo@gmail.com>
2023-08-06 21:53:35 +0300

    Add sprite2D draw

--- src/scene/sprite_2d.cxx
+++ src/scene/sprite_2d.cxx
@@ -4,11 +4,49 @@
 
 #include <iostream>
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
 void Sprite2D::Update() {
 	if (nullptr == m_texture)
 		return;
 
 	uint32_t textureID = m_texture->ID();
 
-	App::GetInstance().Renderer().PushQuad(m_position.x, m_position.y, 0.f, m_texture->Width(), m_texture->Height(), 1.f, 1.f, 1.f, 1.f, 0.f, static_cast<float>(textureID), 0.f);
+	glm::mat4 model(1.f);
+	model = glm::translate(model, {m_position.x, m_position.y, 0.f});
+	model = glm::rotate(model, glm::radians(m_rotation), {0.f, 0.f, 1.f});
+	model = glm::scale(model, {m_scale.x, m_scale.y, 1.f});
+	model = glm::scale(model, {m_texture->Width(), m_texture->Height(), 1.f});
+
+	glm::vec4 points[4] = {
+		{-0.5f, 0.5f, 0.f, 1.f},
+		{-0.5f, -0.5f, 0.f, 1.f},
+		{0.5f, -0.5f, 0.f, 1.f},
+		{0.5f, 0.5f, 0.f, 1.f}};
+	glm::vec2 uvs[4] = {
+		{0.f, 1.f},
+		{0.f, 0.f},
+		{1.f, 0.f},
+		{1.f, 1.f}};
+
+	BatchVertex vertices[4];
+	for (int i = 0; i < 4; i++) {
+		points[i] = model * points[i];
+
+		vertices[i].x = points[i].x;
+		vertices[i].y = points[i].y;
+		vertices[i].z = m_zIndex;
+		vertices[i].r = 1.f;
+		vertices[i].g = 1.f;
+		vertices[i].b = 1.f;
+		vertices[i].a = 1.f;
+		vertices[i].u = uvs[i].x;
+		vertices[i].v = uvs[i].y;
+		vertices[i].t_id = textureID;
+		vertices[i].d_id = 0.f;
+		vertices[i].draw_mode = 0.f;
+	}
+
+	App::GetInstance().Renderer().PushQuad(vertices);
 }
